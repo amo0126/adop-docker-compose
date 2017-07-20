d74472fe07cb62052ceb37b921324f86b8b2ea0b
gilxa1226 <mike.haller@gmail.com>
2017-07-20 11:12:56 -0400

    initial Azure support, very rudimentary

--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+###
+### TODO: Now that initial Azure support is included, need to make a pass through and globalize
+###       certain variables, Region has already been done, however I think there are others that 
+###       could be done. Refactoring will also be necessary, there is a lot of duplication being
+###       done in the file.
+###
+
 echo ' 
       ###    ########   #######  ########  
      ## ##   ##     ## ##     ## ##     ## 
@@ -24,10 +31,17 @@ Usage:
 	    -t aws 
 	    -m <MACHINE_NAME> 
 	    -c <AWS_VPC_ID> 
-	    -r <AWS_DEFAULT_REGION> 
+	    -r <DEFAULT_REGION> 
 	    [-z <AVAILABILITY_ZONE_LETTER>] 
 	    [-a <AWS_ACCESS_KEY>] 
 	    [-s <AWS_SECRET_ACCESS_KEY>] 
+	    [-u <INITIAL_ADMIN_USER>] 
+	    [-p <INITIAL_ADMIN_PASSWORD>]
+
+        ./quickstart.sh 
+	    -t azure 
+	    -m <MACHINE_NAME> 
+	    -r <DEFAULT_REGION> 
 	    [-u <INITIAL_ADMIN_USER>] 
 	    [-p <INITIAL_ADMIN_PASSWORD>]
 
@@ -94,14 +108,14 @@ source_aws() {
     sed -i'' -e "s/###AWS_SUBNET_ID###/$AWS_SUBNET_ID/g" ${AWS_FILE}
   fi
   
-  sed -i'' -e "s/###AWS_DEFAULT_REGION###/$AWS_DEFAULT_REGION/g" ${AWS_FILE}
+  sed -i'' -e "s/###DEFAULT_REGION###/$DEFAULT_REGION/g" ${AWS_FILE}
 
 }
 
 provision_aws() {
     if [ -z ${MACHINE_NAME} ] | \
        [ -z ${AWS_VPC_ID} ] | \
-       [ -z ${AWS_DEFAULT_REGION} ]; then
+       [ -z ${DEFAULT_REGION} ]; then
         echo "ERROR: Mandatory parameters missing!"
         usage
         exit 1
@@ -152,14 +166,109 @@ provision_aws() {
                     --amazonec2-instance-type ${AWS_DOCKER_MACHINE_SIZE} \
                     --amazonec2-root-size ${AWS_ROOT_SIZE:-32}"
 
-        if [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${AWS_DEFAULT_REGION}" ]; then
+        if [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${DEFAULT_REGION}" ]; then
             MACHINE_CREATE_CMD="${MACHINE_CREATE_CMD} \
                         --amazonec2-access-key ${AWS_ACCESS_KEY_ID} \
                         --amazonec2-secret-key ${AWS_SECRET_ACCESS_KEY} \
-                        --amazonec2-region ${AWS_DEFAULT_REGION}"
+                        --amazonec2-region ${DEFAULT_REGION}"
         fi
 
         MACHINE_CREATE_CMD="${MACHINE_CREATE_CMD} ${MACHINE_NAME}"
+        ${MACHINE_CREATE_CMD}
+    fi
+}
+
+# Function to create Azure-specific environment variables file
+source_azure() {  
+  # TODO: Update to Azure specific information
+  # This currently isn't being used in anyway, need to determine if it's necessary
+  AWS_FILE='./conf/provider/env.provider.azure.sh'
+
+  if [ -f ${AZURE_FILE} ]; then  
+    echo "Your AZURE parameters file already exists, deleting it..."
+    rm -f ${AZURE_FILE}
+  fi
+    
+  echo "Creating a new AZURE variables file..."
+  cp ./conf/provider/examples/env.provider.AZURE.sh.example ${AZURE_FILE}
+
+  # AZURE-specific environment variables
+  if [ -z ${AZURE_VPC_ID} ]; then
+    usage
+    exit 1
+  else    
+    sed -i'' -e "s/###AZURE_VPC_ID###/$AZURE_VPC_ID/g" ${AZURE_FILE}
+  fi
+
+  if [ -z ${AZURE_KEYPAIR} ]; then
+    sed -i'' -e "s/###AZURE_KEYPAIR###/$MACHINE_NAME/g" ${AZURE_FILE}
+  else
+    sed -i'' -e "s/###AZURE_KEYPAIR###/$AZURE_KEYPAIR/g" ${AZURE_FILE}
+  fi
+
+  if [ -z ${AZURE_INSTANCE_TYPE} ]; then
+    sed -i'' -e "s/###AZURE_INSTANCE_TYPE###/t2.large/g" ${AZURE_FILE}
+  else
+    sed -i'' -e "s/###AZURE_INSTANCE_TYPE###/$AZURE_INSTANCE_TYPE/g" ${AZURE_FILE}
+  fi
+
+  if [ -z ${AZURE_SUBNET_ID} ]; then
+    sed -i'' -e "s/###AZURE_SUBNET_ID###/default/g" ${AZURE_FILE}
+  else
+    sed -i'' -e "s/###AZURE_SUBNET_ID###/$AZURE_SUBNET_ID/g" ${AZURE_FILE}
+  fi
+  
+  sed -i'' -e "s/###DEFAULT_REGION###/$DEFAULT_REGION/g" ${AZURE_FILE}
+
+}
+
+provision_azure() {
+  # Very basic right now, no way to change certain values, unless variables are pre-exported
+  # into the environment. Definite opportunities for updates and making things smarter.
+    if [ -z ${MACHINE_NAME} ] | \
+       [ -z ${AZURE_SUB_ID} ] | \
+       [ -z ${DEFAULT_REGION} ]; then
+        echo "ERROR: Mandatory parameters missing!"
+        usage
+        exit 1
+    fi
+
+    if [ -z ${AZURE_DOCKER_MACHINE_SIZE} ]; then
+    	export AZURE_DOCKER_MACHINE_SIZE="Standard_D5_v2"
+    fi
+
+    if [ -z ${AZURE_IMAGE} ]; then
+      export AZURE_IMAGE="canonical:UbuntuServer:16.04.0-LTS:latest"
+    fi
+
+    # Create a file with AZURE parameters
+    # source_azure
+
+    # Allow script to continue if error returned by docker-machine command
+    set +e
+
+    # Create Docker machine if one doesn't already exist with the same name
+    docker-machine ip ${MACHINE_NAME} > /dev/null 2>&1
+    rc=$?
+    
+    # Reenable errexit
+    set -e
+    
+    if [ ${rc} -eq 0 ]; then
+        echo "Docker machine '$MACHINE_NAME' already exists"
+    else
+        MACHINE_CREATE_CMD="docker-machine create \
+                    --driver azure \
+                    --azure-subscription-id ${AZURE_SUB_ID} \
+                    --azure-image ${AZURE_IMAGE} \
+                    --azure-location ${DEFAULT_REGION} \
+                    --azure-size ${AZURE_DOCKER_MACHINE_SIZE} \
+                    --azure-open-port 80 \
+                    --azure-open-port 443 \
+                    --engine-install-url=https://web.archive.org/web/20170623081500/https://get.docker.com"
+
+        MACHINE_CREATE_CMD="${MACHINE_CREATE_CMD} ${MACHINE_NAME}"
+        echo ${MACHINE_CREATE_CMD}
         ${MACHINE_CREATE_CMD}
     fi
 }
@@ -185,7 +294,7 @@ while getopts "t:m:a:s:c:z:r:u:p:" opt; do
       export VPC_AVAIL_ZONE=${OPTARG}
       ;;
     r)
-      export AWS_DEFAULT_REGION=${OPTARG}
+      export DEFAULT_REGION=${OPTARG}
       ;;
     u)
       export INITIAL_ADMIN_USER=${OPTARG}
@@ -217,6 +326,9 @@ case ${MACHINE_TYPE} in
     "aws")
         provision_aws
         CLI_COMPOSE_OPTS="-f etc/aws/default.yml"
+        ;;
+    "azure")
+        provision_azure
         ;;
     *)
         echo "Invalid parameter(s) or option(s)."
